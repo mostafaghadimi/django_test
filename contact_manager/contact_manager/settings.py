3be18c62b550627bdacad7510c3fb82d1321f023
Mostafa Ghadimi <mostafa.ghadimi@yahoo.com>
2020-11-26 03:06:42 +0330

    user model customized

--- contact_manager/contact_manager/settings.py
+++ contact_manager/contact_manager/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for contact_manager project.
 
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'apps.users',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'users.User'
