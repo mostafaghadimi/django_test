2d89c4eebef58f4821e33d75df056fe7012536e8
Mostafa Ghadimi <mostafa.ghadimi@yahoo.com>
2020-11-26 09:24:19 +0330

    cant create a new contact

--- contact_manager/apps/contacts/serializers.py
+++ contact_manager/apps/contacts/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
+from rest_framework.fields import CurrentUserDefault
 
 from apps.contacts.models import Contact
 from apps.users.models import User
 
 
 class ContactSerializer(serializers.ModelSerializer):
-    owner = serializers.PrimaryKeyRelatedField(
-        queryset=User.objects.all(),
-    )
+    owner = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='owner')
+
 
     class Meta:
         model = Contact
